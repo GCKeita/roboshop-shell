229db8b1a82e668327d1c9edf42ecc30b37b2fbe
kwetach <kwetach@yahoo.fr>
2023-06-26 22:40:39 -0400

    RoboShop nginx script

---
+++ mysql.sh
@@ -0,0 +1,15 @@
+echo -e "\e[33m Disabling MySQL Default version \e[0m"
+yum module disable mysql -y &>>/tmp/roboshop.log
+
+echo -e "\e[33m Installing MySQL Community Server \e[0m"
+yum install mysql-community-server -y &>>/tmp/roboshop.log
+
+echo -e "\e[33m Starting MySQL Service \e[0m"
+systemctl enable mysqld &>>/tmp/roboshop.log
+systemctl start mysqld &>>/tmp/roboshop.log
+
+echo -e "\e[33m Setup Password \e[0m"
+mysql_secure_installation --set-root-pass RoboShop@1 &>>/tmp/roboshop.log
+
+
+
