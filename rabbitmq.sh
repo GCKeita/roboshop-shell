64b853a68cebb11071964ff541b2cdf12f75e9eb
kwetach <kwetach@yahoo.fr>
2023-06-28 23:23:47 -0400

    shipping script

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,20 +1,18 @@
-echo -e "\e[33m Configureing Repos File \e[0m"
+echo -e "\e[33m Configuring Erlang Repos \e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>/tmp/roboshop.log
 
-echo -e "\e[33m\e[0m"
+echo -e "\e[33m Configuring Rabbitmq Repos \e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>/tmp/roboshop.log
 
-echo -e "\e[33m\e[0m"
+echo -e "\e[33m Installing Rabbitmq server \e[0m"
 yum install rabbitmq-server -y &>>/tmp/roboshop.log
 
-echo -e "\e[33m\e[0m"
+echo -e "\e[33m Starting Rabbitmq Service \e[0m"
 systemctl enable rabbitmq-server &>>/tmp/roboshop.log
 systemctl start rabbitmq-server &>>/tmp/roboshop.log
 
-echo -e "\e[33m\e[0m"
+echo -e "\e[33m adding Rabbitmq Application User \e[0m"
 rabbitmqctl add_user roboshop roboshop123 &>>/tmp/roboshop.log
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>/tmp/roboshop.log
-
-
 
 
