9d9547acd3cc191932b22c936a527b226f123d60
kwetach <kwetach@yahoo.fr>
2023-07-03 12:20:55 -0400

    Catalogue Script

--- catalogue.sh
+++ catalogue.sh
@@ -14,9 +14,9 @@ rm -rf /app &>>/tmp/roboshop.log
 mkdir /app
 
 echo -e "\e[33mDownloading Application content\e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop.log
+curl -o /tmp/component.zip https://roboshop-artifacts.s3.amazonaws.com/component.zip &>>/tmp/roboshop.log
 cd /app
-unzip /tmp/catalogue.zip &>>/tmp/roboshop.log
+unzip /tmp/component.zip &>>/tmp/roboshop.log
 
 echo -e "\e[33mExtracting Application Content\e[0m"
 cd /app
@@ -25,12 +25,12 @@ echo -e "\e[33mInstalling NodeJS Dependencies\e[0m"
 npm install &>>/tmp/roboshop.log
 
 echo -e "\e[33mSetup SystemD Service\e[0m"
-cp /root/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
+cp /root/roboshop-shell/catalogue.service /etc/systemd/system/component.service &>>/tmp/roboshop.log
 
 echo -e "\e[33mStarting Catalogue service\e[0m"
 systemctl daemon-reload &>>/tmp/roboshop.log
-systemctl enable catalogue &>>/tmp/roboshop.log
-systemctl restart catalogue &>>/tmp/roboshop.log
+systemctl enable component &>>/tmp/roboshop.log
+systemctl restart component &>>/tmp/roboshop.log
 
 echo -e "\e[33mCopying MongoDB Repo file\e[0m"
 cp /root/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
@@ -39,7 +39,7 @@ echo -e "\e[33mInstalling MongoDB Client\e[0m"
 yum install mongodb-org-shell -y &>>/tmp/roboshop.log
 
 echo -e "\e[33mLoading Schema\e[0m"
-mongo --host Mmongodb-dev.gckeita-devops.com </app/schema/catalogue.js &>>/tmp/roboshop.log
+mongo --host Mmongodb-dev.gckeita-devops.com </app/schema/component.js &>>/tmp/roboshop.log
 
 
 
