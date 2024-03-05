d2cd5160471b2a83116980119bf393c05e9aecb7
GCKeita <gckeita24@gmail.com>
2024-03-05 01:10:31 -0500

    dispatch.sh

--- dispatch.sh
+++ dispatch.sh
@@ -1,29 +1,26 @@
-dnf install golang -y
+echo -e "${color} Installing GoLang ${nocolor}"
+dnf install golang -y &>>/tmp/roboshop.log
 
-useradd roboshop
+echo -e "${color} Add application user ${nocolor}"
+useradd roboshop &>>/tmp/roboshop.log
 
+echo -e "${color} Make application directory ${nocolor}"
 mkdir /app
 
-curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip
-
+echo -e "${color} Downloading Application Content ${nocolor}"
+curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip &>>/tmp/roboshop.log
 cd /app
 
+echo -e "${color} Extracting Application Content ${nocolor}"
+unzip /tmp/dispatch.zip &>>/tmp/roboshop.log
 
-unzip /tmp/dispatch.zip
+echo -e "${color} Installing Application Dependencies ${nocolor}"
+go mod init dispatch &>>/tmp/roboshop.log
+go get &>>/tmp/roboshop.log
+go build &>>/tmp/roboshop.log
 
-
-go mod init dispatch
-
-
-go get
-
-
-go build
-
-
-systemctl daemon-reload
-
-systemctl enable dispatch
-
-systemctl start dispatch
+echo -e "${color} Starting Dispatch Service ${nocolor}"
+systemctl daemon-reload &>>/tmp/roboshop.log
+systemctl enable dispatch &>>/tmp/roboshop.log
+systemctl restart dispatch &>>/tmp/roboshop.log
 
