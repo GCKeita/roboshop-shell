8b8e8f7b8e4a48730cda692350503c67199be876
GCKeita <124757635+GCKeita@users.noreply.github.com>
2024-07-07 01:42:44 -0400

    rabbitmq.sh

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -17,7 +17,7 @@ systemctl enable rabbitmq-server &>>/tmp/roboshop.log
 systemctl start rabbitmq-server &>>/tmp/roboshop.log
 stat_check $?
 
-echo -e "\${color} adding Rabbitmq Application User ${nocolor}"
+echo -e "${color} adding Rabbitmq Application User ${nocolor}"
 rabbitmqctl add_user roboshop $1 &>>/tmp/roboshop.log
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>/tmp/roboshop.log
 stat_check $?
