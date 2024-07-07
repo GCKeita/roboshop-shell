0fcb6f6d5204ffd3a28dff412c5c185196531358
GCKeita <124757635+GCKeita@users.noreply.github.com>
2024-07-07 01:40:08 -0400

    mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -1,21 +1,26 @@
 source common.sh
 
+
 echo -e "${color} Disabling MySQL Default version ${nocolor}"
 yum module disable mysql -y &>>/tmp/roboshop.log
 stat_check $?
+
 
 echo -e "${color} Copying MySQL Repo File ${nocolor}"
 cp /root/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo &>>/tmp/roboshop.log
 stat_check $?
 
+
 echo -e "${color} Installing MySQL Community Server ${nocolor}"
 yum install mysql-community-server -y &>>/tmp/roboshop.log
 stat_check $?
+
 
 echo -e "${color} Starting MySQL Service ${nocolor}"
 systemctl enable mysqld &>>/tmp/roboshop.log
 systemctl start mysqld &>>/tmp/roboshop.log
 stat_check $?
+
 
 echo -e "${color} Setup Password ${nocolor}"
 mysql_secure_installation --set-root-pass $1 &>>/tmp/roboshop.log
